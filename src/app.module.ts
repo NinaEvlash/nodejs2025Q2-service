156e254440acbeeb548d53180d33fef27fe404dc
Нина Евлаш <evlash.nina@mail.ru>
2025-11-27 18:51:13 +0100

    feat: add all modules, rename user/users module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,19 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { UserModule } from './user/user.module';
-
+import { UsersModule } from './users/user.module';
+import { AlbumsModule } from './albums/albums.module';
+import { ArtistsModule } from './artists/artists.module';
+import { FavoritesModule } from './favorites/favorites.module';
+import { TracksModule } from './tracks/tracks.module';
 @Module({
-  imports: [UserModule],
+  imports: [
+    UsersModule,
+    AlbumsModule,
+    ArtistsModule,
+    FavoritesModule,
+    TracksModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
