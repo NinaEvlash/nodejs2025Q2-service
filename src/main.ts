8714294adb54b627b5714de6d8e481becaf2a500
Нина Евлаш <evlash.nina@mail.ru>
2025-11-29 14:47:45 +0100

    feat: integrate OpenAPI doc and setup Swagger UI in project

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as swaggerUi from 'swagger-ui-express';
+import * as YAML from 'yamljs';
+import { join } from 'path';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(4000);
+  const swaggerDocument = YAML.load(join(__dirname, '..', 'doc', 'api.yaml'));
+
+  app.use('/api', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
+  await app.listen(process.env.PORT || 4000);
 }
 bootstrap();
